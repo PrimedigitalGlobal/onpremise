6ed6dc410523c969af0b21c0d294ca99d8167631
Vikalp Jain <mevikalp@gmail.com>
2020-09-06 03:41:10 +0530

    remove smtp, redis and postgres

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ exec &> >(tee -a "$log_file")
 
 MIN_DOCKER_VERSION='17.05.0'
 MIN_COMPOSE_VERSION='1.23.0'
-MIN_RAM=2400 # MB
+MIN_RAM=1500 # MB
 
 SENTRY_CONFIG_PY='sentry/sentry.conf.py'
 SENTRY_CONFIG_YML='sentry/config.yml'
@@ -95,8 +95,6 @@ fi
 echo ""
 echo "Creating volumes for persistent storage..."
 echo "Created $(docker volume create --name=sentry-data)."
-echo "Created $(docker volume create --name=sentry-postgres)."
-echo "Created $(docker volume create --name=sentry-redis)."
 echo "Created $(docker volume create --name=sentry-zookeeper)."
 echo "Created $(docker volume create --name=sentry-kafka)."
 echo "Created $(docker volume create --name=sentry-clickhouse)."
@@ -228,27 +226,6 @@ fi
 echo "Bootstrapping and migrating Snuba..."
 $dcr snuba-api bootstrap --force
 echo ""
-
-# Very naively check whether there's an existing sentry-postgres volume and the PG version in it
-if [[ $(docker volume ls -q --filter name=sentry-postgres) && $(docker run --rm -v sentry-postgres:/db busybox cat /db/PG_VERSION 2>/dev/null) == "9.5" ]]; then
-    docker volume rm sentry-postgres-new || true
-    # If this is Postgres 9.5 data, start upgrading it to 9.6 in a new volume
-    docker run --rm \
-    -v sentry-postgres:/var/lib/postgresql/9.5/data \
-    -v sentry-postgres-new:/var/lib/postgresql/9.6/data \
-    tianon/postgres-upgrade:9.5-to-9.6
-
-    # Get rid of the old volume as we'll rename the new one to that
-    docker volume rm sentry-postgres
-    docker volume create --name sentry-postgres
-    # There's no rename volume in Docker so copy the contents from old to new name
-    # Also append the `host all all all trust` line as `tianon/postgres-upgrade:9.5-to-9.6`
-    # doesn't do that automatically.
-    docker run --rm -v sentry-postgres-new:/from -v sentry-postgres:/to alpine ash -c \
-     "cd /from ; cp -av . /to ; echo 'host all all all trust' >> /to/pg_hba.conf"
-    # Finally, remove the new old volume as we are all in sentry-postgres now
-    docker volume rm sentry-postgres-new
-fi
 
 echo ""
 echo "Setting up database..."
